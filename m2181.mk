26ac51f770a04721a66a1e889ac0e33e34a0fce7
baizhi958216 <1475289190@qq.com>
2022-03-02 10:35:58 +0800

    m2181: USB: Import needed host and accessory permissions
    * Stock rom has that xmls..
    
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- m2181.mk
+++ m2181.mk
@@ -85,6 +85,10 @@ $(call inherit-product, vendor/qcom/opensource/usb/vendor_product.mk)
 TARGET_HAS_DIAG_ROUTER := true
 TARGET_KERNEL_VERSION := 5.4
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml
+
 # Vibrator
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
