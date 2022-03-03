3921c7bb39b711161045d86b101b104b5c286185
baizhi958216 <1475289190@qq.com>
2022-03-03 17:48:27 +0800

    Revert "m2181: Make Android.mk search recursive"
    This reverts commit 011c4c1c461828dd1c90649cb738d853efe39f2d.

--- Android.mk
+++ Android.mk
@@ -1,6 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),m2181)
-  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
 endif
