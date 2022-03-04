fef67f3e91ea69669dbdf5a2ef44c550b6cd24f7
baizhi958216 <1475289190@qq.com>
2022-03-04 21:18:29 +0800

    m2181: Enable updating of APEXes
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- m2181.mk
+++ m2181.mk
@@ -5,6 +5,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 # Enable Scoped Storage related
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Call the proprietary setup
 $(call inherit-product, vendor/meizu/m2181/m2181-vendor.mk)
 
