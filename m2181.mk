1cec0263458b99e0d40d4fd4ee064a0b43bc2416
Edwin Moquete <edwinmmoquete@gmail.com>
2022-03-04 14:22:16 -0400

    m2181: Import public.libraries.txt
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- m2181.mk
+++ m2181.mk
@@ -250,6 +250,10 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock
 
+# Additional native libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
