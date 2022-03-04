eea0cd55074d923d5baa545ee6c624cbcc1a6cf1
baizhi958216 <1475289190@qq.com>
2022-03-04 21:19:06 +0800

    m2181: Setup dalvik vm configs
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- m2181.mk
+++ m2181.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Call the proprietary setup
 $(call inherit-product, vendor/meizu/m2181/m2181-vendor.mk)
 
