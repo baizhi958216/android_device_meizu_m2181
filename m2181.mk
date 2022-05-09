8b69b2008a281ce938107113e4771dae4192d233
baizhi958216 <1475289190@qq.com>
2022-05-09 20:50:18 +0800

    m2181: Dalvik: Use phone-xhdpi-6144-dalvik-heap.mk * Same as Stock   ```   dalvik.vm.heapstartsize=16m   dalvik.vm.heapgrowthlimit=256m   dalvik.vm.heapsize=512m   dalvik.vm.heaptargetutilization=0.5   dalvik.vm.heapminfree=8m   dalvik.vm.heapmaxfree=32m   ```
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- m2181.mk
+++ m2181.mk
@@ -13,6 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Call the proprietary setup
 $(call inherit-product, vendor/meizu/m2181/m2181-vendor.mk)
 
