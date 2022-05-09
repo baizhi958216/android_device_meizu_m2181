14466ff0388ab6d2285950c6d299e9e2ff58be10
baizhi958216 <1475289190@qq.com>
2022-05-09 16:54:04 +0800

    m2181: Import scripts from meizu/meizu_18_CN/meizu18:11/RKQ1.210715.001/1646967620:user/release-keys
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- m2181.mk
+++ m2181.mk
@@ -13,6 +13,49 @@ PRODUCT_COPY_FILES += $(M2181_PREBUILT)/dtb:dtb.img
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.crda.sh \
+    init.kernel.post_boot-lahaina.sh \
+    init.kernel.post_boot-shima.sh \
+    init.kernel.post_boot-yupik.sh \
+    init.kernel.post_boot.sh \
+    init.mdm.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.chg_policy.sh \
+    init.qti.display_boot.sh \
+    init.qti.kernel.debug-lahaina.sh \
+    init.qti.kernel.debug-shima.sh \
+    init.qti.kernel.debug-yupik.sh \
+    init.qti.kernel.debug.sh \
+    init.qti.kernel.sh \
+    init.qti.media.sh \
+    init.qti.qcv.sh \
+    qca6234-service.sh \
+    vendor_modprobe.sh \
+
+PRODUCT_PACKAGES += \
+    fstab.default \
+    init.qcom.factory.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.qti.kernel.rc \
+    init.qti.ufs.rc \
+    init.target.rc \
+    init.recovery.qcom.rc \
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk/fstab.qcom
+
 # Vendor_boot modules
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(M2181_PREBUILT)/modules/vendor_boot,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules)
