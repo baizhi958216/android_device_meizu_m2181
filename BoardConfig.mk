53ccdafe73e6cbe6f9f593d87936863280ab7a71
baizhi958216 <1475289190@qq.com>
2022-05-09 13:14:51 +0800

    m2181: Prebuilt kernel for m2181
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,7 @@
 BOARD_VENDOR := meizu
 
 DEVICE_PATH := device/meizu/m2181
+M2181_PREBUILT := device/meizu/prebuilt/m2181
 
 # Architecture
 TARGET_ARCH := arm64
@@ -20,6 +21,32 @@ TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a75
+
+# Kernel
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0 androidboot.memcg=1
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 service_locator.enable=1
+BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3 swiotlb=0
+BOARD_KERNEL_CMDLINE += loop.max_part=7 cgroup.memory=nokmem,nosocket
+BOARD_KERNEL_CMDLINE += pcie_ports=compat loop.max_part=7
+BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
+BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
+BOARD_KERNEL_IMAGE_NAME := kernel
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
+TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
+
+TARGET_PREBUILT_KERNEL := $(M2181_PREBUILT)/kernel
+TARGET_PREBUILT_DTB := $(M2181_PREBUILT)/dtb
+BOARD_PREBUILT_DTBOIMAGE := $(M2181_PREBUILT)/dtbo.img
+TARGET_FORCE_PREBUILT_KERNEL := true
+TARGET_KERNEL_CONFIG := vendor/lahaina_GKI
+TARGET_KERNEL_SOURCE := kernel/meizu/sm8350
 
 # Inherit from proprietary files
 include vendor/meizu/m2181/BoardConfigVendor.mk
--- m2181.mk
+++ m2181.mk
@@ -3,3 +3,16 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+M2181_PREBUILT := device/meizu/prebuilt/m2181
+
+# DTB
+PRODUCT_COPY_FILES += $(M2181_PREBUILT)/dtb:dtb.img
+
+# Vendor_boot modules
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(M2181_PREBUILT)/modules/vendor_boot,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules)
+
+# Vendor modules
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(M2181_PREBUILT)/modules/vendor,$(TARGET_COPY_OUT_VENDOR)/lib/modules)
