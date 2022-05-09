d42e82c1b6a6b99606f0c7753ad715aa4429faf9
baizhi958216 <1475289190@qq.com>
2022-05-09 13:07:48 +0800

    m2181: Setup architecture for m2181 * from LA.UM.9.14.r1-19400-LAHAINA.QSSI13.0
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,5 +8,18 @@ BOARD_VENDOR := meizu
 
 DEVICE_PATH := device/meizu/m2181
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := kryo300
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a75
+
 # Inherit from proprietary files
 include vendor/meizu/m2181/BoardConfigVendor.mk
