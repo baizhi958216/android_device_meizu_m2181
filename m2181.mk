f337cf19bff64e520e5bc98f527cab9a46310b1d
baizhi958216 <1475289190@qq.com>
2022-05-10 16:51:15 +0800

    m2181: hidl: Build ```hidltransport``` ```hwbinder```
    * Some old hals still need them
    
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- m2181.mk
+++ m2181.mk
@@ -173,7 +173,11 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0.vendor
+    android.hidl.manager@1.0.vendor \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # Keymaster
 PRODUCT_PACKAGES += \
