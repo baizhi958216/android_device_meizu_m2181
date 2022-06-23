f7dea299bc799671f721ec370c104896bc0c4f79
baizhi958216 <1475289190@qq.com>
2022-06-23 21:57:14 +0800

    m2181: rootdir: Convert recovery configuration file
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -6,15 +6,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-# Recovery init configuration files
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.qcom.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
