b61241e082c230cc13730b94045eb46a3dcc3347
baizhi958216 <1475289190@qq.com>
2022-07-04 11:45:52 +0800

    m2181: recovery: Change to recovery.fstab * Move out init.recovery.qcom.rc
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- m2181.mk
+++ m2181.mk
@@ -313,6 +313,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     fstab.default \
+    recovery.fstab \
     init.qcom.factory.rc \
     init.qcom.rc \
     init.qti.kernel.rc \
@@ -321,8 +322,7 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
 # Sensors
 PRODUCT_PACKAGES += \
