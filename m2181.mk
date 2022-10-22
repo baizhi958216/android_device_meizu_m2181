b10980b7c76bd603cf1dfeef8d18afe886e1ad05
baizhi958216 <1475289190@qq.com>
2022-10-22 19:46:35 +0800

    m2181: Build vendor.qti.hardware.btconfigstore@1.0.vendor F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti": library "vendor.qti.hardware.btconfigstore@1.0.so" not found: needed by main executable
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- m2181.mk
+++ m2181.mk
@@ -83,6 +83,7 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     libbluetooth_audio_session \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_COPY_FILES += \
